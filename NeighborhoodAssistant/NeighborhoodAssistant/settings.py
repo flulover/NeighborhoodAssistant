6c1aa90d18fe41616acb351e4d79dc0078b8caa2
flulover <twer@yzzhou.local>
2013-10-22 00:41:43 +0800

    Create a app named business and set up the mysql db connection and db name. Add admin website to our project.

--- NeighborhoodAssistant/NeighborhoodAssistant/settings.py
+++ NeighborhoodAssistant/NeighborhoodAssistant/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'neighborhood_assistant',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
@@ -121,7 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'business'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
